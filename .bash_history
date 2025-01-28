7b23e3d4b482fb51820288a4837301fc5a618f07
Ashley Margetts <amargett.mit.edu>
2025-01-28 10:48:01 -0500

    animation gif

--- .bash_history
+++ .bash_history
@@ -1,1 +1,7 @@
 git status
+git add .
+git commit -m "cleanup"
+git config --global user.email "amargett.mit.edu"
+git config --global user.name "Ashley Margetts"
+git commit -m "cleanup"
+git push
--- PRBM/prbm_joints.m
+++ PRBM/prbm_joints.m
@@ -5,6 +5,10 @@ theta_max = pi/4; % Maximum angle
 num_frames = 100; % Number of animation frames
 theta_vals = linspace(theta0, theta_max, num_frames);
 
+% GIF parameters
+gif_filename = 'prbm_animation.gif'; % Output GIF file name
+delay_time = 0.1; % Delay between frames (in seconds)
+
 % Example: Two-link manipulator (replace with your robot's kinematics)
 lfull = 50; 
 ltip = 3.25; 
@@ -42,7 +46,9 @@ link7_plot = plot([0, 0], [0, 0], 'g-', 'LineWidth', 2); % Link 3
 joint_plot = plot(0, 0, 'ko', 'MarkerSize', 4, 'MarkerFaceColor', 'k'); % Joints
 
 % Animation loop
-for theta = theta_vals
+for frame = 1:num_frames
+    theta = theta_vals(frame); 
+    
     theta2 = theta + pi - theta0; 
     theta3 = theta + pi/2 -theta0; 
     ihat = [1 0]; jhat = [0 1];
@@ -75,8 +81,22 @@ for theta = theta_vals
     set(joint_plot, 'XData', [r1(1), r2(1), r3(1), r4(1), r5(1), r6(1)],...
         'YData', [r1(2), r2(2), r3(2), r4(2), r5(2), r6(2)]);
    
-    % Pause for animation
-    pause(0.1);
+    % Capture the frame
+    frame_image = getframe(gcf);
+    im = frame2im(frame_image); % Convert to image
+    
+    % Write to GIF
+    [imind, cm] = rgb2ind(im, 256); % Convert to indexed image
+    if frame == 1
+        % Create the GIF file on the first frame
+        imwrite(imind, cm, gif_filename, 'gif', 'LoopCount', Inf, 'DelayTime', delay_time);
+    else
+        % Append to the GIF file
+        imwrite(imind, cm, gif_filename, 'gif', 'WriteMode', 'append', 'DelayTime', delay_time);
+    end
+    
+    % Pause for real-time animation (optional)
+    pause(delay_time);
 end
 
-disp('Animation Complete!');
+disp(['Animation saved as ', gif_filename]);
